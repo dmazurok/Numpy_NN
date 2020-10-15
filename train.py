1702ec3d609e670c85b2e8b97a83f9b0b5b88902
Dmitrii Mazurok <mazurok007@gmail.com>
2020-10-15 22:50:54 +0300

    added empty train.py and empty utils/config_reader.py

---
+++ train.py
@@ -0,0 +1,7 @@
+from model import model
+from utils.general_functions import *
+from utils.activations import *
+from utils.cost_functions import *
+from utils.config_reader import config_reader
+
+
---
+++ utils/config_reader.py
@@ -0,0 +1,5 @@
+class config_reader():
+    def __init__(config_file):
+        self.config_file = config_file
+    def get_config():
+        return 
