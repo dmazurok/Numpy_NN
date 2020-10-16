3bec2b8edf36a32f62b1284635e4fa1ad2215fb4
Dmitrii Mazurok <mazurok007@gmail.com>
2020-10-17 01:02:36 +0300

    implemented config_reader class, added model_config.json, empty evaluate.py

--- train.py
+++ train.py
@@ -4,4 +4,6 @@ from utils.activations import *
 from utils.cost_functions import *
 from utils.config_reader import config_reader
 
+config_path = 'model_parameters.json'
+
 
--- utils/config_reader.py
+++ utils/config_reader.py
@@ -1,5 +1,13 @@
+import json
+
 class config_reader():
-    def __init__(config_file):
+    def __init__(self, config_file):
         self.config_file = config_file
-    def get_config():
-        return 
+        self.config = None
+    def get_config(self):
+        if not self.config:
+            self.config = json.load(open(self.config_file, 'r'))
+        return self.config
+    def get_model_param_n_per_layer(self): # model_params: [{input_dim:N, output_dim:N}...]
+        config = self.get_config()
+        return [config['layers'][i][0:2] for i in config['layers']]
